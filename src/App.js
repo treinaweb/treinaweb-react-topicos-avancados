dad0d394ee18722e23e0200723691411434d98e6
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-26 11:23:21 -0200

    aula-03-01

--- src/App.js
+++ src/App.js
@@ -1,54 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import Index from './views/index';
+import About from './views/about';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      myList: []
     }
 
-    this.add = this.add.bind(this);
-    this.remove = this.remove.bind(this);
-  }
-
-  add(){
-    this.setState(state => ({
-      myList: [...state.myList, {id: Date.now()}]
-    }));
-  }
-
-  remove(){
-    const myList = this.state.myList;
-    if(myList.length){
-      myList.splice(0, 1);
-      this.setState({myList});
-    }
   }
 
   render() {
     const { state } = this;
     return (
       <div>
-        <button onClick={this.add} >ADD</button>
-        <button onClick={this.remove} >REMOVE</button>
-        <TransitionGroup>
-          {
-            state.myList.map(item => {
-              return <CSSTransition key={item.id} timeout={300} classNames={{
-                enter: 'entrando',
-                exitActive: 'saindo'
-              }} >
-                <div className={'btn'} >{item.id}</div>
-              </CSSTransition>
-            })
-          }
-          
-        </TransitionGroup>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/" >Home</Link>
+                </li>
+                <li>
+                  <Link to="/about/" >About</Link>
+                </li>
+              </ul>
+            </nav>
+            <Route path="/" exact component={Index} />
+            <Route path="/about/" component={About} />
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Index = () => <h2>Home</h2>
+
+export default Index;
