0d5a1a2facaa31d0bf9c04d18e22bca90729e252
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-26 10:43:38 -0200

    aula-02-04

--- src/App.js
+++ src/App.js
@@ -2,40 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import CSSTransition from 'react-transition-group/CSSTransition';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      isOn: true
+      myList: []
     }
 
-    this.handleClick = this.handleClick.bind(this);
+    this.add = this.add.bind(this);
+    this.remove = this.remove.bind(this);
   }
 
-  handleClick(){
-    this.setState(({isOn}) => {
-      return {
-        isOn: !isOn
-      }
-    })
+  add(){
+    this.setState(state => ({
+      myList: [...state.myList, {id: Date.now()}]
+    }));
+  }
+
+  remove(){
+    const myList = this.state.myList;
+    if(myList.length){
+      myList.splice(0, 1);
+      this.setState({myList});
+    }
   }
 
   render() {
     const { state } = this;
     return (
       <div>
-        <CSSTransition in={state.isOn} timeout={{
-          enter: 300,
-          exit: 500
-        }} classNames={{
-          enter: 'entrando',
-          exitDone: 'saindo'
-        }} >
-          <button onClick={this.handleClick} className={'btn'} >CLICK</button>
-        </CSSTransition>
+        <button onClick={this.add} >ADD</button>
+        <button onClick={this.remove} >REMOVE</button>
+        <TransitionGroup>
+          {
+            state.myList.map(item => {
+              return <CSSTransition key={item.id} timeout={300} classNames={{
+                enter: 'entrando',
+                exitActive: 'saindo'
+              }} >
+                <div className={'btn'} >{item.id}</div>
+              </CSSTransition>
+            })
+          }
+          
+        </TransitionGroup>
       </div>
     );
   }
