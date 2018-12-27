584041065593f3cc9a03f9d35d509221d4ea0bbb
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-27 09:56:27 -0200

    aula-05-01

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import React, { Component, Suspense } from 'react';
+// @flow
+
+import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Prompt, Switch } from 'react-router-dom';
-import loadable from '@loadable/component';
 
-import About from './views/about';
-import Address from './views/about/address';
-
-const Index = loadable(() => import('./views/index'));
-const TextCounter = React.lazy(() => import('./components/TextCounter'));
 
 class App extends Component {
 
@@ -22,28 +17,7 @@ class App extends Component {
     const { state } = this;
     return (
       <div>
-        <Router>
-          <div>
-            <Prompt when={false} message={(location) => `Tem certeza que deseja ir para ${location.pathname}?`} />
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/" >Home</Link>
-                </li>
-                <li>
-                  <Link to="/about/" >About</Link>
-                </li>
-              </ul>
-            </nav>
-            <Suspense fallback={<div>Loading...</div>} >
-              <Switch>
-                <Route path="/" exact component={() => <Index />} />
-                <Route path="/about" component={About} />
-                <Route path="/:abc" component={Address} />
-              </Switch>
-            </Suspense>
-          </div>
-        </Router>
+        <h1>Hello!</h1>
       </div>
     );
   }
