f85b70a79ac8368a7468d3dd69abe31ed5e32e0a
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-28 08:49:57 -0200

    aula-07-03

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import enLocaleData from 'react-intl/locale-data/en';
 import jaLocaleData from 'react-intl/locale-data/ja';
 import ptLocaleData from 'react-intl/locale-data/pt';
-import { addLocaleData, IntlProvider, FormattedNumber } from 'react-intl';
+import { addLocaleData, IntlProvider, FormattedDate, FormattedTime } from 'react-intl';
 
 import ClickCounter from './components/ClickCounter';
 
@@ -28,7 +28,9 @@ class App extends Component {
       <div>
         <IntlProvider locale={state.currentLang} >
           <div>
-            <FormattedNumber value={123456789.12} />
+            <FormattedDate value={new Date('2020-12-31T15:00:00')} />
+            <br />
+            <FormattedTime value={new Date('2020-12-31T15:00:00')} />
           </div>
         </IntlProvider>
       </div>
