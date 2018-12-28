fb8e2f3440ed8c39e0ba66129059e50af99c2628
Akira Hanashiro <felipehanashiro@hotmail.com>
2018-12-28 08:45:55 -0200

    aula-07-02

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import enLocaleData from 'react-intl/locale-data/en';
 import jaLocaleData from 'react-intl/locale-data/ja';
 import ptLocaleData from 'react-intl/locale-data/pt';
-import { addLocaleData, IntlProvider } from 'react-intl';
+import { addLocaleData, IntlProvider, FormattedNumber } from 'react-intl';
 
 import ClickCounter from './components/ClickCounter';
 
@@ -17,7 +17,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      currentLang: 'pt'
+      currentLang: 'ja'
     }
 
   }
@@ -27,8 +27,9 @@ class App extends Component {
     return (
       <div>
         <IntlProvider locale={state.currentLang} >
-          <h1>Hello!</h1>
-          <ClickCounter />
+          <div>
+            <FormattedNumber value={123456789.12} />
+          </div>
         </IntlProvider>
       </div>
     );
