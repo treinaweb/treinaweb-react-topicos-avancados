073dc58ca0644860ce507b1e1f89f8dc60219733
Akira Hanashiro <felipehanashiro@hotmail.com>
2019-01-10 10:21:54 -0200

    projeto-03-01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import NewProductView from './views/NewProductView';
@@ -8,8 +9,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NewProductView />
-        <ProductListView />
+        <Router>
+            <div>
+              <header>
+                <ul className="link-list" >
+                  <li>
+                    <Link to={'/'} >Novo</Link>
+                  </li>
+                  <li>
+                    <Link to={'/list'} >Lista</Link>
+                  </li>
+                </ul>
+              </header>
+              <div>
+                <Route path={'/'} exact component={NewProductView} />
+                <Route path={'/list'} component={ProductListView} />
+              </div>
+            </div>
+        </Router>
       </div>
     );
   }
