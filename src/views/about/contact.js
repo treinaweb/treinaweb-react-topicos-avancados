3e0233ba8190905f1c37b9e6b489629b6447a685
hanashiro <felipeaah@gmail.com>
2020-01-27 09:01:49 -0300

    aula-03-06

--- src/views/about/contact.js
+++ src/views/about/contact.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import {useHistory, useLocation, useParams, useRouteMatch} from 'react-router-dom';
 
-const Contact = () => <h2>Contact</h2>
+const Contact = () => {
+    //const history = useHistory();
+    //history.push('/')
+
+    //const location = useLocation();
+    //console.log(location);
+
+    //const params = useParams();
+    //console.log(params);
+
+    //const match = useRouteMatch('/about/contact/123/treinaweb/');
+    //console.log(match)
+
+    return (
+        <h2>Contact</h2>
+    )
+}
 
 export default Contact;
